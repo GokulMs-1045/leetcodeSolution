eea6aaa632bce2a3ec200de7a1526a0ad5807aa7
Gokul <gokunavi2004@gmail.com>
2025-06-19 16:34:46 +0530

    Time: 5 ms (28.01%), Space: 44.7 MB (17.11%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,26 +1,20 @@
 class Solution {
-
-    private int func(int row, int col, int limit, List<List<Integer>> triangle, int[][] dp){
-
-        if(row == limit)
-            return triangle.get(row).get(col);
-
-        if(dp[row][col] != - 1)
-            return dp[row][col];
-
-        int down = triangle.get(row).get(col) + func(row + 1, col, limit, triangle, dp);
-        int downRight = triangle.get(row).get(col) + func(row + 1, col + 1, limit, triangle, dp);
-
-        return dp[row][col] = Math.min(down, downRight);
-    }
-
     public int minimumTotal(List<List<Integer>> triangle) {
-        int m = triangle.size() - 1;
-        int dp[][] = new int[m][m];
+         int n = triangle.size();
 
-        for(int[] row : dp)
-            Arrays.fill(row, - 1);
+        int[][] dp = new int[n][n];
 
-        return func(0, 0, m, triangle, dp);
+        for(int i = 0 ; i < n ; i++)
+            dp[n - 1][i] = triangle.get(n - 1).get(i);
+
+        for(int row = n - 2 ; row >= 0 ; row--){
+            for(int col = row ; col >= 0 ; col--){
+
+                int down = triangle.get(row).get(col) + dp[row + 1][col];
+                int diagonal = triangle.get(row).get(col) + dp[row + 1][col + 1];
+                dp[row][col] = Math.min(down, diagonal);
+            }
+        }
+        return dp[0][0];
     }
 }
