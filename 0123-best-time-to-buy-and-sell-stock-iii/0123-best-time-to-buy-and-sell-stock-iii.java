eaa697e95985f7cf6e4b92f8d87ddbf872908fc9
Gokul <gokunavi2004@gmail.com>
2025-06-19 21:20:06 +0530

    Time: 58 ms (42.43%), Space: 63.8 MB (38.31%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,44 +1,27 @@
 class Solution {
-
-        private int func(int ind, int buy, int cap, int n, int[] arr, int[][][] dp){
-
-        if(ind == n || cap == 0)
-            return 0;
-
-        if (dp[ind][buy][cap] != -1) 
-            return dp[ind][buy][cap];
-        
-
-        int profit = 0;
-
-        if(buy == 0)
-            profit = Math.max(0 + func(ind + 1, 0, cap, n, arr, dp), (-1) * arr[ind] + func(ind + 1, 1, cap, n, arr, dp));
-
-        if(buy == 1)
-            profit = Math.max(0 + func(ind + 1, 1, cap, n, arr, dp), arr[ind] + func(ind + 1, 0, cap - 1, n, arr, dp));
-
-        return dp[ind][buy][cap] = profit;
-    }
-
     public int maxProfit(int[] prices) {
 
         int n = prices.length;
-
-         if (n == 0) 
-            return 0;
-
-        int[][][] dp = new int[n][2][3];
         
-        // Initialize the dp array with -1
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
+        int[][][] dp = new int[n + 1][2][3];
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            
+            for (int buy = 0; buy <= 1; buy++) {
+
+                for (int cap = 1; cap <= 2; cap++) {
+
+                    if (buy == 0) 
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][0][cap],
+                                                     (-1)*prices[ind] + dp[ind + 1][1][cap]);
+                    
+                    if (buy == 1) 
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][1][cap],
+                                                     prices[ind] + dp[ind + 1][0][cap - 1]);
+                }
             }
         }
 
-        int ans = func(0, 0, 2, n, prices, dp);
-        
-        // Return the maximum profit
-        return ans;
+        return dp[0][0][2];
     }
 }
