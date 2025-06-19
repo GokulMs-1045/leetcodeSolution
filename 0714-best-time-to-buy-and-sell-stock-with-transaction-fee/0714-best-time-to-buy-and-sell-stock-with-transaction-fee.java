0ee37b7d811d7877bcfe0e8b82e71860ec32baae
Gokul <gokunavi2004@gmail.com>
2025-06-19 21:25:10 +0530

    Time: 17 ms (46.68%), Space: 56.7 MB (31.62%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,43 +1,31 @@
 class Solution {
-
-    private int func(int ind, int buy, int fee, int n, int[] arr, int[][] dp){
-
-        if(ind == n)
-            return 0;
-
-        if (dp[ind][buy] != -1) 
-            return dp[ind][buy];
-        
-
-        int profit = 0;
-
-        if(buy == 0)
-            profit = Math.max(0 + func(ind + 1, 0, fee, n, arr, dp), (-1) * arr[ind] + func(ind + 1, 1, fee, n, arr, dp));
-
-        if(buy == 1)
-            profit = Math.max(0 + func(ind + 1, 1, fee, n, arr, dp), arr[ind] - fee + func(ind + 1, 0, fee, n, arr, dp));
-
-        return dp[ind][buy] = profit;
-    }
-
     public int maxProfit(int[] prices, int fee) {
 
         int n = prices.length;
 
-        if (n == 0) 
+        if(n == 0)
             return 0;
+
+        int[][] dp = new int[n + 1][2];
+
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+
+        int profit = 0;
+
+        for(int ind = n - 1 ; ind >= 0 ; ind--){
+            for(int buy = 0 ; buy <= 1 ; buy++){
+
+                if (buy == 0) 
+                    profit = Math.max(0 + dp[ind + 1][0], (-1)*prices[ind] + dp[ind + 1][1]);
             
-        // Declare a DP table to memoize results
-        int[][] dp = new int[n][2];
-        
-        // Initialize the dp array with -1
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+                if (buy == 1) 
+                    profit = Math.max(0 + dp[ind + 1][1], prices[ind] - fee + dp[ind + 1][0]);
+
+                dp[ind][buy] = profit;
+            }
         }
 
-        int ans = func(0, 0, fee, n, prices, dp);
-        
-        // Return the maximum profit
-        return ans;
+        return dp[0][0];
     }
 }
