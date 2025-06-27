030dfdd37f9e18b8964a8c5e7c850c0d820ca3dd
Gokul <gokunavi2004@gmail.com>
2025-06-27 11:42:42 +0530

    Time: 2 ms (83.23%), Space: 45.4 MB (58.45%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,72 @@
+class Solution {
+
+    private int[] delRow = {-1, 0, 1, 0};
+    private int[] delCol = {0, 1, 0, -1};
+    
+    /* Helper Function to check if 
+    a cell is within boundaries */
+    private boolean isValid(int i, int j, 
+                            int n, int m) {
+                                
+        // Return false if cell is invalid
+        if (i < 0 || i >= n) return false;
+        if (j < 0 || j >= m) return false;
+        
+        // Return true if cell is valid
+        return true;
+    }
+
+    private void dfs(int row, int col, 
+                    boolean[][] vis, 
+                    char[][] board, 
+                    int n, int m) {
+                        
+        vis[row][col] = true;
+        
+        for (int i = 0; i < 4; i++) {
+            
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+            
+            if (isValid(nRow, nCol, n, m) && 
+                board[nRow][nCol] == 'O' && 
+                !vis[nRow][nCol]) {
+                    
+                dfs(nRow, nCol, vis, board, n, m);
+            }
+        }
+    }
+    public void solve(char[][] board) {
+         int n = board.length;
+        int m = board[0].length;
+
+        boolean[][] vis = new boolean[n][m];
+
+        for (int j = 0; j < m; j++) {
+            
+            // First row
+            if (!vis[0][j] && board[0][j] == 'O') 
+                dfs(0, j, vis, board, n, m);
+            
+            if (!vis[n - 1][j] && board[n - 1][j] == 'O')
+                dfs(n - 1, j, vis, board, n, m);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            // First column
+            if (!vis[i][0] && board[i][0] == 'O')
+                dfs(i, 0, vis, board, n, m);
+            
+            if (!vis[i][m - 1] && board[i][m - 1] == 'O') 
+                dfs(i, m - 1, vis, board, n, m);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                
+                if (board[i][j] == 'O' && !vis[i][j])
+                    board[i][j] = 'X';
+            }
+        }
+    }
+}
