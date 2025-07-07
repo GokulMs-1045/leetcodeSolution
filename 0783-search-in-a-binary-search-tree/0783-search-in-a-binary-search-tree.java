d0aa9b34711dea76981764cebb22daf86c3bb9dc
Gokul <gokunavi2004@gmail.com>
2025-07-07 11:55:05 +0530

    Time: 0 ms (100%), Space: 45.6 MB (20.3%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -30,4 +30,11 @@ class Solution {
 
         return root;
     }
+
+        // while (root != null && root.data != val) {
+
+        //     root = (root.data > val) ? root.left : root.right;
+        // }
+
+        // return root; 
 }
